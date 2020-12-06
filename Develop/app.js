63bfe7be40a30c6e9bb83a06f40c0650332551c3
MikeFogz <MikeFogz@gmail.com>
2020-12-05 16:58:36 -0800

    Wrote Employee Class

--- Develop/app.js
+++ Develop/app.js
@@ -11,14 +11,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
-// Write code to use inquirer to gather information about the development team members,
+//1) Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
+//2) After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
+//3) After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
@@ -33,3 +33,11 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+    //Position
+    {type: "list", name: "appTitle", message: "Name of your Application",},
+
+    {type: "list", name: "appLicense", message: "What is your role?", choices: ['MIT', 'Apache', 'GPL', 'None']},
+    if (typeof role = intern 
+    
